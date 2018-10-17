93d7b17ec8a58b8ced0b7703f93ecc6b0abcc32d
Seongeun Yu <johong130801@gmail.com>
2018-10-17 10:35:02 +0900

    feat: urls.py 수정하고 템플릿, 뷰 수정중

--- school_project/info/urls.py
+++ school_project/info/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'info'
 urlpatterns = [
     path('', views.list_of_schools, name="list_of_schools"),
     path('school/<int:school_id>/', views.school_details, name="school_details"),
     path('student/', views.list_of_students, name="list_of_students"),
-    path('student/<int:student.id>/', views.student_details, name="student_details"),
+    path('student/<int:student_id>/', views.student_details, name="student_details"),
 ]
