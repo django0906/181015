332aca5b7f920b8f029b79faf25deb7366f7d8e5
Seongeun Yu <johong130801@gmail.com>
2018-10-17 20:19:04 +0900

    feat: 템플릿 수정 완료. 뷰와 템플릿 수정하면 끝.

--- school_project/info/models.py
+++ school_project/info/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+__all__ = (
+    'School',
+    'Students',
+)
+
 
 class School(models.Model):
     school_name = models.CharField(max_length=16)
@@ -11,4 +16,11 @@ class Students(models.Model):
         on_delete=models.CASCADE,
     )
     student_name = models.CharField(max_length=10)
-    friend_name_list = models.ManyToManyField("self")
+    friend_name_list = models.ManyToManyField('self')
+
+    def __str__(self):
+        # 이한영 (친구: a, b, c)
+        #  QuerySet순회 및 문자열 포매팅
+        friend_list = self.friend_name_list.all()
+        friend_list_str = ', '.join([friend.student_name for friend in friend_list])
+        return f'{self.student_name} (친구: {friend_list_str})'
